49a547830b63baf2620ac822cd5092fe3c877140
Bill Curtis <7672827+billcurtis@users.noreply.github.com>
2023-06-20 09:29:14 -0400

    Init Commit

---
+++ migrate-loadstate.ps1
@@ -0,0 +1,25 @@
+﻿
+
+$accountName = "domaina0\wcurtis"
+$newAcctName = "domainb0\wcurtis"
+$usmtPath = "C:\USMT"
+$profileSavePath = "C:\tempProfile"
+
+Set-Location -LiteralPath $usmtPath | Out-Null
+
+
+
+if (!(Test-Path $profileSavePath)) {
+
+New-Item -Path $profileSavePath -ItemType Directory -Force -Confirm:$false | Out-Null
+
+}
+
+$scanStatePath = (Get-ChildItem -Path $usmtPath -Recurse | Where-Object {$_.Name -eq "scanstate.exe"}).FullName
+$loadStatePath = (Get-ChildItem -Path $usmtPath -Recurse | Where-Object {$_.Name -eq "loadstate.exe"}).FullName
+
+
+$expression = "$loadStatePath $profileSavePath /ue:$newAcctName /i:migdocs.xml /i:migapp.xml /mu:$($accountName):$($newAcctName)  /i:MigUser.xml /i:MigChrome.xml"
+Invoke-Expression -Command $expression
+
+
---
+++ migrate-scanstate.ps1
@@ -0,0 +1,22 @@
+﻿
+
+$accountName = "domaina0\wcurtis"
+$usmtPath = "C:\USMT"
+$profileSavePath = "C:\tempProfile"
+
+Set-Location -LiteralPath $usmtPath | Out-Null
+
+if (!(Test-Path $profileSavePath)) {
+
+New-Item -Path $profileSavePath -ItemType Directory -Force -Confirm:$false | Out-Null
+
+}
+
+$scanStatePath = (Get-ChildItem -Path $usmtPath -Recurse | Where-Object {$_.Name -eq "scanstate.exe"}).FullName
+$loadStatePath = (Get-ChildItem -Path $usmtPath -Recurse | Where-Object {$_.Name -eq "loadstate.exe"}).FullName
+
+
+$expression = "$scanStatePath $profileSavePath /ue:*\* /i:migdocs.xml /i:migapp.xml /o /ui:$accountName /config:Config_AppsAndSettings.xml /i:MigUser.xml /i:MigChrome.xml /c"
+Invoke-Expression -Command $expression
+
+
